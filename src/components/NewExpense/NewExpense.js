2a3c39e55054652dda3011a43c9a71843258e6e3
Rosie Gardner <rosiegardner78@gmail.com>
2022-10-17 14:54:55 -0700

    create: folder for new expense form components

---
+++ src/components/NewExpense/NewExpense.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '.././../styles/NewExpense/NewExpense.css'
+
+const NewExpense = () => {
+  return (
+    <div>
+      <form>
+
+      </form>
+    </div>
+  )
+}
+
+export default NewExpense;
